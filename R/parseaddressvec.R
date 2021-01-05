cd7aa4d0b737a281c7c328e71aa9aea6621ea880
Neal <nbarsch@colgate.edu>
2021-01-04 20:45:35 -0700

    Update parseaddressvec.R

--- R/parseaddressvec.R
+++ R/parseaddressvec.R
@@ -5,8 +5,9 @@
 #' @param return option to return single address characteristic, i.e. Zipcode
 #' @export
 parseaddressvec <- function(addressvec, return="all"){
-  library(tfwstring)
-  library(data.table)
+  require(tfwstring)
+  require(data.table)
+  require(dplyr)
   #adf <- data.frame(adnum=c(1:length(addressvec)),address=as.character(addressvec),stringsAsFactors = F)
   dflist <- lapply(c(1:length(addressvec)),FUN=function(x){data.frame(parseaddress(addressvec[x], return="horizontal"),stringsAsFactors = F)})
   dfres <- rbindlist(dflist, fill=T)
