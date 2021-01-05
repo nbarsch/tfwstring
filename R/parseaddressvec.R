0888479be59d56c1fc3b9dfab61725b7816733f1
Neal <nbarsch@colgate.edu>
2021-01-04 20:43:07 -0700

    Create parseaddressvec.R
    create parseaddressvec

---
+++ R/parseaddressvec.R
@@ -0,0 +1,22 @@
+#' parseaddressvec()
+#'
+#' Get address parts from a string addresses in a vector 
+#' @param addressvec string vector of addresses to parse, to use a df column use df_name$address_column_name
+#' @param return option to return single address characteristic, i.e. Zipcode
+#' @export
+parseaddressvec <- function(addressvec, return="all"){
+  library(tfwstring)
+  library(data.table)
+  #adf <- data.frame(adnum=c(1:length(addressvec)),address=as.character(addressvec),stringsAsFactors = F)
+  dflist <- lapply(c(1:length(addressvec)),FUN=function(x){data.frame(parseaddress(addressvec[x], return="horizontal"),stringsAsFactors = F)})
+  dfres <- rbindlist(dflist, fill=T)
+  dfres %>% mutate_all(as.character)->dfres
+  if(return=="all"){
+    return(rbindlist(dflist, fill=T))
+  }else{
+    dfres$tecreturn <- dfres[,get(return)]
+    return(dfres$tecreturn)
+  }
+  
+}
+
