0562d6a2113072ea9b3e6bd072f9644e125c96f6
Neal <nbarsch@colgate.edu>
2021-04-06 05:59:20 -0600

    ad ex3
    ad ex3

---
+++ R/extxt3.R
@@ -0,0 +1,32 @@
+#' extxt3()
+#' extract parts from a string based on defined string split
+#' @param string string to split on to find what to extract
+#' @param ps page source in text unlisted to find what to extract 
+#' @param nchar number of characters from located string forward to extract
+#' @param firstonly if TRUE returns only first instance of extract
+#' @export
+extxt3 <- function(string,ps,nchar=100,firstonly=F){
+  startchar <- str_locate_all(ps,string)[[1]]
+  if(length(startchar)>0){
+    
+    if(!isTRUE(firstonly)){
+      sapply(c(1:nrow(startchar)),FUN=function(x){
+        if(isTRUE((startchar[x]+nchar)<nchar(ps))){
+          return(substr(ps,startchar[x],startchar[x]+nchar))
+        }else{
+          return(substr(ps,startchar[x],nchar(ps)))
+          #substr(ps,startchar[x],nchar(startchar[x]))
+          
+        }
+      })
+    }else{
+      if(isTRUE((startchar[x]+nchar)<nchar(ps))){
+        return(substr(ps,startchar[x],startchar[x]+nchar))
+      }else{
+        return(substr(ps,startchar[x],nchar(ps)))
+      }
+    }
+  }else{
+    return("NOT_FOUND")
+  }
+}
