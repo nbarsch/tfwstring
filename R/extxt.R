ee2a56b575e2064f6f6705cc700b7e7945987dd3
Neal <nbarsch@colgate.edu>
2021-04-05 22:22:38 -0600

    extxt up
    extxt up

---
+++ R/extxt.R
@@ -0,0 +1,15 @@
+#' extxt()
+#' extract parts from a string based on defined string split
+#' @param string string to split on to find what to extract
+#' @param ps page source in text unlisted to find what to extract 
+#' @param nchar number of characters from located string forward to extract
+#' @param firstonly if TRUE returns only first instance of extract
+#' @export
+extxt <- function(string,nchar=100,ps,firstonly=F){
+  startchar <- str_locate_all(ps,string)[[1]]
+  if(!isTRUE(firstonly)){
+    return(sapply(c(1:nrow(startchar)),FUN=function(x){substr(ps,startchar[x],startchar[x]+nchar)}))
+  }else{
+    return(substr(ps,startchar[1],startchar[1]+nchar))
+  }
+}
