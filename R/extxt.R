af8fc8753ba4037ed3bf52ed51cf0b2e7a091071
Neal <nbarsch@colgate.edu>
2021-04-06 05:58:10 -0600

    update extxt
    update extxt

--- R/extxt.R
+++ R/extxt.R
@@ -5,23 +5,25 @@
 #' @param nchar number of characters from located string forward to extract
 #' @param firstonly if TRUE returns only first instance of extract
 #' @export
-extxt <- function(string,nchar=100,ps,firstonly=F){
+extxt <- function(string,ps,nchar=100,firstonly=F){
   startchar <- str_locate_all(ps,string)[[1]]
   if(length(startchar)>0){
     
     if(!isTRUE(firstonly)){
-      return(sapply(c(1:nrow(startchar)),FUN=function(x){
-        if(isTRUE((startchar[x]+nchar)<nchar(startchar[x]))){
+      sapply(c(1:nrow(startchar)),FUN=function(x){
+        if(isTRUE((startchar[x]+nchar)<nchar(ps))){
           return(substr(ps,startchar[x],startchar[x]+nchar))
         }else{
-          return(substr(ps,startchar[x],nchar(startchar[x])))
+          return(substr(ps,startchar[x],nchar(ps)))
+          #substr(ps,startchar[x],nchar(startchar[x]))
+          
         }
-      }))
+      })
     }else{
-      if(isTRUE((startchar[x]+nchar)<nchar(startchar[x]))){
+      if(isTRUE((startchar[x]+nchar)<nchar(ps))){
         return(substr(ps,startchar[x],startchar[x]+nchar))
       }else{
-        return(substr(ps,startchar[x],nchar(startchar[x])))
+        return(substr(ps,startchar[x],nchar(ps)))
       }
     }
   }else{
