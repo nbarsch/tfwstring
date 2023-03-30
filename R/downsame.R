6374c77e2805289d3ecaab8e23623ebda8e2c681
Oddspotato Admin <admin@oddspotato.com>
2023-03-30 04:55:24 -0600

    Update downsame.R

--- R/downsame.R
+++ R/downsame.R
@@ -8,39 +8,33 @@ downsame <- function(xpath){
   tl1 <- length(gh(xpath=xpath))
   webElem <- remDr$findElement("css", "body")
   webElem$sendKeysToElement(list(key = "end"))
-  rs()
+  rs(min=2,max=3)
   #top of page
   webElem <- remDr$findElement("css", "body")
   webElem$sendKeysToElement(list(key = "home"))
-  rs()
+  rs(min=2,max=3)
   #scroll to bottom of page
   webElem <- remDr$findElement("css", "body")
   webElem$sendKeysToElement(list(key = "end"))
-  rs()
-  #top of page
-  webElem <- remDr$findElement("css", "body")
-  webElem$sendKeysToElement(list(key = "home"))
-  rs()
+  rs(min=2,max=3)
   tl2 <- length(gh(xpath=xpath))
   while(tl1!=tl2){
+    #scroll to bottom of page
     tl1 <- length(gh(xpath=xpath))
     webElem <- remDr$findElement("css", "body")
     webElem$sendKeysToElement(list(key = "end"))
-    rs()
+    rs(min=2,max=3)
     #top of page
     webElem <- remDr$findElement("css", "body")
     webElem$sendKeysToElement(list(key = "home"))
-    rs()
+    rs(min=2,max=3)
     #scroll to bottom of page
     webElem <- remDr$findElement("css", "body")
     webElem$sendKeysToElement(list(key = "end"))
-    rs()
-    #top of page
-    webElem <- remDr$findElement("css", "body")
-    webElem$sendKeysToElement(list(key = "home"))
-    rs()
+    rs(min=2,max=3)
     tl2 <- length(gh(xpath=xpath))
   }
+  webElem <- remDr$findElement("css", "body")
+  webElem$sendKeysToElement(list(key = "home"))
+  Sys.sleep(.3)
 }
-
-
