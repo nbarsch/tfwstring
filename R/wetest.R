99e5698606d428f1a38fd0ca9ac8367a2fb97293
Oddspotato Admin <admin@oddspotato.com>
2023-04-04 15:20:05 -0600

    update wetest

--- R/wetest.R
+++ R/wetest.R
@@ -3,16 +3,24 @@
 #' web element test
 #' @param xpath xpath of webelem
 #' @export
-wetest <- function(xpath){
+wetest2 <- function(xpath){
   remDr <- get("remDr",envir=globalenv())
   webElemtest <-NULL
+  wer <- 1
   while(is.null(webElemtest)){
     webElemtest <- suppressMessages(tryCatch({remDr$findElement(using = 'xpath', xpath)},
                                              error = function(e){NULL}))
+    rs(.2,.5)
+    wer <- wer+1
+    if(wer>30){
+      webElemtest <- "NF"
+    }
     #loop until element with name <value> is found in <webpage url>
   }
+
   randsleep <- sample(seq(.01, .02, by = 0.001), 1)
   Sys.sleep(randsleep)
+
 }
 
 
