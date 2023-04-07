35d9bca3652ebe16c1afbea3b8e794f85430e6fe
Oddspotato Admin <admin@oddspotato.com>
2023-04-07 05:17:06 -0600

    update ceclickopen
    update ceclickopen

--- R/ceclickopen.R
+++ R/ceclickopen.R
@@ -5,10 +5,13 @@
 ceclickopen <- function(){
   remDr <- get("remDr")
   listicon <- remDr$findElements(using="xpath","//button[@class='listIconWrapper']")
-  clickall(elems=listicon)
-  rs(.1,.2)
-
+  if(length(listicon)>0){
+    clickall(elems=listicon)
+    rs(.1,.2)
+  }
   viewmore <- remDr$findElements(using="xpath","//button[text()='View More']")
-  clickall(elems=viewmore)
-  rs(.1,.2)
+  if(length(viewmore)>0){
+    clickall(elems=viewmore)
+    rs(.1,.2)
+  }
 }
